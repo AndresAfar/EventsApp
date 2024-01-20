7350ac4438f8a356b0bc16c4f39ffa7512debd2b
AndresAfar <afar2503@gmail.com>
2024-01-19 22:43:22 -0500

    modified style footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer className="bg-[#0f172a] rounded-lg shadow m-4 dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 flex justify-center">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <span className="text-base text-gray-100 sm:text-center dark:text-gray-400">
                 © 2023 <a href="https://flowbite.com/" className="hover:underline">EventsView</a>. All Rights Reserved.
             </span>
         </div>
